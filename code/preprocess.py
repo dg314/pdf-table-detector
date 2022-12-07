90a7a9b3f7615c453f7a52b0df8985e4cc4282cc
dg314 <david_grossman@brown.edu>
2022-12-06 22:09:51 -0500

    sample dataset

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,2 +1,4 @@
-def get_data():
-    pass
+def get_data(data_path="../data/docbank-samples/"):
+    train_inputs, train_labels, test_inputs, test_labels = None, None, None, None
+    
+    return train_inputs, train_labels, test_inputs, test_labels
