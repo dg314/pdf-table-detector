3646767a360fb33745554a55c87fec9fee2de504
spacegoat1 <yash_b_mehta@brown.edu>
2022-12-14 21:28:57 -0500

    code refactoring

--- code/rpn_features.py
+++ code/rpn_features.py
@@ -6,7 +6,7 @@ from tensorflow.keras.applications.vgg16 import preprocess_input
 import hyperparameters
 
 
-def calc_rpn(labels,resized_width, resized_height):
+def generate_roi_props(labels,resized_width, resized_height):
     downscale = float(16)
     anchor_sizes = hyperparameters.anchor_box_scales
     anchor_ratios = hyperparameters.anchor_box_ratios
@@ -172,25 +172,8 @@ def get_anchor_gt(images, labels):
             image_data = images[index]
             label_data = np.expand_dims(labels[index], axis=0) # We expect labels to be (n, 4) in shape
             try:
-                # try:
-                y_rpn_cls, y_rpn_regr, num_pos = calc_rpn(label_data, resized_width, resized_height)
-                # except Exception as e:
-                #     print(f"Error with calc_rpn: {e}")
-                #     continue
-
-                # Zero-center by mean pixel, and preprocess image
-                # Check https://stackoverflow.com/questions/50213159/preprocess-input-in-keras-increase-the-size-of-train-drastically
-                # Also check https://stackoverflow.com/questions/47555829/preprocess-input-method-in-keras -> loading images with keras is better
-
+                y_rpn_cls, y_rpn_regr, _ = generate_roi_props(label_data, resized_width, resized_height)
                 image_data = preprocess_input(image_data)
-                # image_data = np.expand_dims(image_data, axis=0)
-
-                # image_data = image_data[:,:, (2, 1, 0)]  # BGR -> RGB
-                # image_data = image_data.astype(np.float32)
-                # image_data[:, :, 0] -= 103.939
-                # image_data[:, :, 1] -= 116.779
-                # image_data[:, :, 2] -= 123.68
-                # image_data /= 1.0
 
                 image_data = np.transpose(image_data, (2, 0, 1))
                 image_data = np.expand_dims(image_data, axis=0)
@@ -198,9 +181,9 @@ def get_anchor_gt(images, labels):
                 # y_rpn_regr is a concat of y_rpn_overlap (x4) and y_rpn_regr. Looks like we are converting y_rpn_regr vals to float
                 y_rpn_regr[:, y_rpn_regr.shape[1]//2:, :, :] *= 1.0
 
-                image_data = np.transpose(image_data, (0, 2, 3, 1)) # converting to RGB again?
-                y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1)) # Convert (2 x 9 x 16 x 16) to (2 x 16 x 16 x 9) again ???
-                y_rpn_regr = np.transpose(y_rpn_regr, (0, 2, 3, 1)) # Convert (2 x 9 x 16 x 16) to (2 x 16 x 16 x 9) again ???
+                image_data = np.transpose(image_data, (0, 2, 3, 1)) 
+                y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1)) # Convert (2 x 9 x 16 x 16) to (2 x 16 x 16 x 9)
+                y_rpn_regr = np.transpose(y_rpn_regr, (0, 2, 3, 1)) # Convert (2 x 9 x 16 x 16) to (2 x 16 x 16 x 9)
 
                 yield np.copy(image_data), [np.copy(y_rpn_cls), np.copy(y_rpn_regr)], label_data, index
 
